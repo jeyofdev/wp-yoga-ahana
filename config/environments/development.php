7d5da17b26da364288a18e9ac83845a2e1ea1754
jeyofdev <jgregoire.pro@gmail.com>
2020-07-19 10:27:35 +0200

    add debug tools

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,7 +3,9 @@
  * Configuration overrides for WP_ENV === 'development'
  */
 
+use Whoops\Run;
 use Roots\WPConfig\Config;
+use Whoops\Handler\PrettyPageHandler;
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
@@ -12,6 +14,11 @@ Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 
 ini_set('display_errors', '1');
+
+// php errors handler
+$whoops = new Run;
+$whoops->pushHandler(new PrettyPageHandler);
+$whoops->register();
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
