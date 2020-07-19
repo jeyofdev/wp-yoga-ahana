305e9a4aaad6f93651b48638fa82c993e1259b54
jeyofdev <jgregoire.pro@gmail.com>
2020-07-19 11:06:07 +0200

    initialize timber

---
+++ web/app/themes/yogaahana/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+use Timber\Timber;
+
+
+
+// timber
+$timber = new Timber();
+Timber::$autoescape = true;
+Timber::$dirname = "views";
--- web/app/themes/yogaahana/index.php
+++ web/app/themes/yogaahana/index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
+<?php
 
-    <body>
-        <p>Hello world !!!</p>
-    </body>
-</html>
+use Timber\Timber;
+
+
+
+/**
+ * The main template file
+ */
+
+$context = Timber::context();
+$templates = "pages/index.twig";
+
+
+
+Timber::render($templates, $context);
