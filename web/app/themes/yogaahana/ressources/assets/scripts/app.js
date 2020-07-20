62e5aa027dd2fae14f6b3fab0607ab50df1ac207
jeyofdev <jgregoire.pro@gmail.com>
2020-07-20 09:57:07 +0200

    add the assets

--- web/app/themes/yogaahana/ressources/assets/scripts/app.js
+++ web/app/themes/yogaahana/ressources/assets/scripts/app.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+// jquery
+import $ from 'jquery'
+
+// lib
+import 'bootstrap'
+import 'slicknav/dist/jquery.slicknav'
+import 'owl.carousel'
+import 'jquery-ui'
+import 'magnific-popup'
+import './lib/jquery.nice-select.min.js'
+
+// app
+import './app/main.js'
